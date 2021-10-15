98ff6a44f664f378af07901aa29508467068e4f9
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 14:57:42 +0200

    Copy from the ticket

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -4,20 +4,20 @@ const github = require("@actions/github");
 async function run() {
     try {
         const issueTitle = core.getInput("issue-title");
-        const issueBody = core.getInput("joke");
+        const jokeBody = core.getInput("joke");
         const token = core.getInput("repo-token");
 
         const octokit = github.getOctokit(token);
 
         const newIssue = await octokit.issues.create({
-            title: issueTitle,
-            body: issueBody,
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
+            title: issueTitle,
+            body; jokeBody
         });
     } catch (err) {
         core.setFailed(err.message);
     }
 }
 
-run ();
+run()
