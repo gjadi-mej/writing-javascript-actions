abed055719a36b3d994ffd534e654db087d27262
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 15:24:00 +0200

    Remove more more async

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -11,8 +11,8 @@ const options = {
     json: true
 };
 
-async function getJoke() {
-    const res = await request(options);
+function getJoke() {
+    const res = request(options);
     return res.joke;
 }
 
