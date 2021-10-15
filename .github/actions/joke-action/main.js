6724d144f1a613938ab2869a43c3d5a9fc294005
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 15:12:25 +0200

    Test for core.setOutput with hardcoded value

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -2,6 +2,7 @@ const getJoke = require("./joke");
 const core = require("@actions/core");
 
 async function run() {
+    core.setOutput('outputKey', 'outputVal');
     const joke = await getJoke();
     console.log(joke);
     core.setOutput("joke-output", joke);
