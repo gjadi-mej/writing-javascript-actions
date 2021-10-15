9c7dae4490032a5ac4966610a90c6a5cff694e90
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 15:01:01 +0200

    add more debug log

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,6 +9,8 @@ async function run() {
 
         const octokit = github.getOctokit(token);
         console.log(octokit);
+        console.log(octokit.rest);
+        console.log(octokit.issues);
         const newIssue = await octokit.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
