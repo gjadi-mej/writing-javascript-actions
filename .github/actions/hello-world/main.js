7fa92fd6533afc8312c8fc63574cf1abe9888412
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 10:43:52 +0200

    Update action and workflow to greet 2 [3] person

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const core = require("@actions/core");
+
+const firstGreeting = code.getInput("first-greeting");
+const secondGreeting = code.getInput("second-greeting");
+const thirdGreeting = code.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+if (thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`);
+}
