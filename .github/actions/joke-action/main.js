5b3b3f95abf28853d843d848d8c13cd0326c084f
Grégoire Jadi <gregoire.jadi@microej.com>
2021-10-15 15:28:49 +0200

    Fix bad merge when reverting async

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -8,12 +8,5 @@ async function run() {
     core.setOutput("joke-output", joke);
 }
 
-<<<<<<< HEAD
-// run();
-core.setOutput('outputKey', 'outputVal');
-const joke = await getJoke();
-console.log(joke);
-core.setOutput("joke-output", joke);
-=======
 run();
->>>>>>> parent of 2eece40... Try without async stuff
+
